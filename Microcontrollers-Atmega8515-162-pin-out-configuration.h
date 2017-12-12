570a422e2f93d40201d82ffc74bd1b120b06977b
Dmitry7777 <list-dmitrij@mail.ru>
2017-12-12 19:05:31 +0700

    Create Microcontrollers-Atmega8515-162-pin-out-configuration.h
    When you edit your firmware, you can type program codes of chip core either not numbers of pins.
    For example, in ARDUINO.
    
    
    #include <LiquidCrystal.h>
    #include"AVR_PINOUT.h"
    
    const int LCD_RS = MCU_PEDI_PORTE_PE2; //
    const int LCD_RW = MCU_PEDI_PORTE_PE1; //
    const int LCD_E = MCU_PEDI_PORTE_PE0; //
    const int LCD_DB0 = MCU_PADI_PORTA_PA0; //
    const int LCD_DB1 = MCU_PADI_PORTA_PA1; //
    const int LCD_DB2 = MCU_PADI_PORTA_PA2; //
    const int LCD_DB3 = MCU_PADI_PORTA_PA3; //
    const int LCD_DB4 = MCU_PADI_PORTA_PA4; //
    const int LCD_DB5 = MCU_PADI_PORTA_PA5; //
    const int LCD_DB6 = MCU_PADI_PORTA_PA6; //
    const int LCD_DB7 = MCU_PADI_PORTA_PA7; //
    
    LiquidCrystal LCD_1602(LCD_RS, LCD_RW, LCD_E, LCD_DB0, LCD_DB1, LCD_DB2, LCD_DB3, LCD_DB4, LCD_DB5, LCD_DB6, LCD_DB7); //
    
    void setup() {
      // put your setup code here, to run once:
      LCD_1602.begin(16, 2); //
      LCD_1602.setCursor(0, 0); //
      LCD_1602.print(""); //
    pinMode(LED, OUTPUT); //
    }
    
    void loop() {
      // put your main code here, to run repeatedly:
    
    }

---
+++ Microcontrollers-Atmega8515-162-pin-out-configuration.h
@@ -0,0 +1,57 @@
+/*Microcontrollers Atmega8515/162 pin out configuration
+*/
+#ifndef AVR_PINOUT_H
+#define AVR_PINOUT_H
+
+#include"AVR_PINOUT.h"
+#else
+#include"Arduino.h"
+#include<avr/io.h>
+#include<avr/signal.h>
+
+/*Interface of Port B*/
+#define MCU_SPI_PBDI_PORTB_PB0 0; //
+#define MCU_SPI_PBDI_PORTB_PB1 1; //
+#define MCU_SPI_PBDI_PORTB_PB2 2; //
+#define MCU_SPI_PBDI_PORTB_PB3 3; //
+#define MCU_SPI_PBDI_PORTB_PB4 4; //
+#define MCU_SPI_PBDI_PORTB_PB5 5; //
+#define MCU_SPI_PBDI_PORTB_PB6 6; //
+#define MCU_SPI_PBDI_PORTB_PB7 7; //
+
+/*Interface of Port D*/
+#define MCU_USART_PDDI_PORTD_PD0 8; //
+#define MCU_USART_PDDI_PORTD_PD1 9; //
+#define MCU_USART_PDDI_PORTD_PD2 10; //
+#define MCU_USART_PDDI_PORTD_PD3 11; //
+#define MCU_USART_PDDI_PORTD_PD4 12; //
+#define MCU_USART_PDDI_PORTD_PD5 13; //
+#define MCU_USART_PDDI_PORTD_PD6 14; //
+#define MCU_USART_PDDI_PORTD_PD7 15; //
+
+/*Interface of Port C*/
+#define MCU_PCDI_PORTC_PC0 16; //
+#define MCU_PCDI_PORTC_PC1 17; //
+#define MCU_PCDI_PORTC_PC2 18; //
+#define MCU_PCDI_PORTC_PC3 19; //
+#define MCU_PCDI_PORTC_PC4 20; //
+#define MCU_PCDI_PORTC_PC5 21; //
+#define MCU_PCDI_PORTC_PC6 22; //
+#define MCU_PCDI_PORTC_PC7 23; //
+
+/*Interface of Port A*/
+#define MCU_PADI_PORTA_PA0 24; //
+#define MCU_PADI_PORTA_PA1 25; //
+#define MCU_PADI_PORTA_PA2 26; //
+#define MCU_PADI_PORTA_PA3 27; //
+#define MCU_PADI_PORTA_PA4 28; //
+#define MCU_PADI_PORTA_PA5 29; //
+#define MCU_PADI_PORTA_PA6 30; //
+#define MCU_PADI_PORTA_PA7 31; //
+
+/*Interface of Port E*/
+#define MCU_PEDI_PORTE_PE2 34; //
+#define MCU_PEDI_PORTE_PE1 33; //
+#define MCU_PEDI_PORTE_PE0 32; //
+
+#endif
